0a8f11ce88419b778110f43e2a49a3dac34d17fc
elbernaderen <65098903+elbernaderen@users.noreply.github.com>
2022-04-05 15:55:50 -0300

    added rsi

---
+++ amplitudes_rsi.py
@@ -0,0 +1,91 @@
+import sys
+import pandas as pd
+from tools import name_col
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+import time
+import pickle
+import datetime
+
+# B is the rise percent of the candles next to the close of the last candle
+#  that we use to decide if this sequence anticipate a rise
+B = float(sys.argv[1])
+
+
+#number of rows,or how many candles we are goingo to analize is p minus in_ 
+p = 15
+# in_ is the number of candles we consider to know if the price rises or fall
+in_ = 2
+def verify(file,nam):
+
+    index_ = name_col(p,in_)
+    index_.append("date")
+    index_.append("buy")
+    t = file.drop(columns= "time")
+    
+    k = pd.DataFrame(columns = index_)
+    for i in range(p,len(file.index)):
+        
+        si_no = 1
+        
+        if ((file["high"][i] - file["close"][i-in_]) / file["close"][i-in_]) > B or ((file["high"][i-1] - file["close"][i-in_]) / file["close"][i-in_]) > B :
+            si_no = f"{B}"
+        else:
+            si_no = "0"
+        if si_no != 1:
+            row = list()
+            for t in range(in_ ,p+1):
+                row.append(file["open"][i-t] / file["close"][i-p])
+                row.append(file["high"][i-t] / file["close"][i-p])
+                row.append(file["low"][i-t] / file["close"][i-p])
+                row.append(file["close"][i-t] / file["close"][i-p])
+                row.append((file["open"][i-t] - file["close"][i-t]) / file["low"][i-t])
+                row.append((file["close"][i-t] - file["open"][i-t]) / file["high"][i-t])
+                row.append((file["open"][i-t] - file["close"][i-t]) / (file["high"][i-t]-file["low"][i-t]))
+                row.append((file["high"][i-t]) / (file["low"][i-t]))
+            row.append(file["date"][i-in_])
+            row.append(si_no)
+            k.loc[len(k.index)] = row
+    #i+=5
+    return k
+
+names=["LISTA:"]
+count = 0
+for nam in range(2,len(sys.argv)):
+    print(str(sys.argv[nam]))
+    file =  pd.read_csv(f"{str(sys.argv[nam])}_1d_basehs.csv")
+
+    if count == 0:
+        v = verify(file,nam = str(sys.argv[nam]))
+        count +=1
+    else:
+        k = verify(file,nam = str(sys.argv[nam]))
+        v = v.append(k,ignore_index=True)
+    names.append(str(sys.argv[nam]))
+
+print(len(v))
+y = v.buy
+
+shib_features =  name_col(p,in_)
+       
+X =  v[shib_features]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,random_state=10)
+rfc = RandomForestClassifier(n_estimators=1000)
+rfc.fit(X_train,y_train)
+predictions = rfc.predict(X_test)
+print(confusion_matrix(y_test,predictions))
+
+st = str(datetime.datetime.now())
+st = st.replace(" ","_")
+st = st.replace(":","_")
+filename = f'model_alta{B}_{st[0:15]}.sav'
+print("rows: ",p-in_)
+pickle.dump(rfc, open(filename, 'wb'))
+print(f'model_alta{B}_{st[0:15]}.sav' )
+print(names)
+print(classification_report(y_test,predictions))
+loaded_model = pickle.load(open(filename, 'rb'))
+comprobacion = loaded_model.predict(X_test)
+
--- tools.py
+++ tools.py
@@ -12,3 +12,29 @@ def name_col(p,in_):
         index_.append(f"ampl_1{cin}")
         index_.append(f"ampl_2{cin}")
     return index_
+    # extracted from https://programmerclick.com/article/34731200625/
+def RSI(t, periods=10):
+    length = len(t)
+    rsies = [np.nan]*length
+         # La longitud de los datos no excede el período y no se puede calcular;
+    if length <= periods:
+        return rsies
+         #Utilizado para cálculos rápidos;
+    up_avg = 0
+    down_avg = 0
+ 
+         # Primero calcule el primer RSI, use los períodos anteriores + 1 dato para formar una secuencia de períodos;
+    first_t = t[:periods+1]
+    for i in range(1, len(first_t)):
+                 #Precio aumentado;
+        if first_t[i] >= first_t[i-1]:
+            up_avg += first_t[i] - first_t[i-1]
+                 #caída de los precios; 
+        else:
+            down_avg += first_t[i-1] - first_t[i]
+    up_avg = up_avg / periods
+    down_avg = down_avg / periods
+    rs = up_avg / down_avg
+    rsies[periods] = 100 - 100/(1+rs)
+
+    return rsies
