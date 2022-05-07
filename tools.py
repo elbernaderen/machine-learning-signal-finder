c6d68fff199cbb4a0bb55cf0616eea909503024c
elbernaderen <65098903+elbernaderen@users.noreply.github.com>
2022-05-07 10:01:11 -0300

    add ignore

--- tools.py
+++ tools.py
@@ -1,17 +1,23 @@
 import numpy as np
-from sklearn.neighbors import radius_neighbors_graph
+import pandas as pd
+#from sklearn.neighbors import radius_neighbors_graph
+
 def name_col(p,in_):
     ind_row = p - in_ + 1
     index_ = list()
     for cin in range(ind_row):
-        index_.append(f"open_{cin}")
-        index_.append(f"high_{cin}")
-        index_.append(f"low_{cin}")
-        index_.append(f"close_{cin}")
+        #index_.append(f"open_{cin}")
+      #  index_.append(f"high_{cin}")
+        #index_.append(f"low_{cin}")
+        #index_.append(f"close_{cin}")
+        index_.append(f"volume_{cin}")
         index_.append(f"mart_{cin}")
         index_.append(f"mart_inv_{cin}")
-        index_.append(f"ampl_1{cin}")
         index_.append(f"ampl_2{cin}")
+        index_.append(f"rsi_{cin}")
+        index_.append(f"macd_{cin}")
+        index_.append(f"macd_h{cin}")
+        index_.append(f"macd_s{cin}")
     return index_
     # extracted from https://programmerclick.com/article/34731200625/
 
@@ -55,3 +61,28 @@ def RSI(t, periods=10):
         rs = up_avg/down_avg
         rsies[j] = 100 - 100/(1+rs)
     return rsies
+def macd(file):
+    df=file
+    k = df['close'].ewm(span=12, adjust=False, min_periods=12).mean()
+# Get the 12-day EMA of the closing price
+    d = df['close'].ewm(span=26, adjust=False, min_periods=26).mean()
+# Subtract the 26-day EMA from the 12-Day EMA to get the MACD
+    macd = k - d
+# Get the 9-Day EMA of the MACD for the Trigger line
+    macd_s = macd.ewm(span=9, adjust=False, min_periods=9).mean()
+# Calculate the difference between the MACD - Trigger for the Convergence/Divergence value
+    macd_h = macd - macd_s
+# Add all of our new values for the MACD to the dataframe
+    df['macd'] = df.index.map(macd)
+    df['macd_h'] = df.index.map(macd_h)
+    df['macd_s'] = df.index.map(macd_s)
+    return df
+
+def name_col_2(in_):
+    ind_row = in_
+    index_ = list()
+    for cin in range(ind_row):
+        index_.append(f"salida_{cin}")
+    for cin in range(ind_row):
+        index_.append(f"salidam_{cin}")
+    return index_
