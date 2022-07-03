1aa92b27bac2eaebec509a76ac08deca113783af
elbernaderen <65098903+elbernaderen@users.noreply.github.com>
2022-07-03 11:31:22 -0300

    modificate configuration

--- amplitudes.py
+++ amplitudes.py
@@ -12,6 +12,7 @@ from scipy.stats import linregress
 
 ######################################################################################################
 # PROGRAM DEFINITIONS
+
 # B is the rise percent of the candles next to the close of the last candle
 #  that we use to decide if this sequence anticipate a rise
 
@@ -20,24 +21,29 @@ B = float(
         "Enter the percentage that have to rise the price to consider it as a success: \n"
     )
 )
+
 # in_ is the number of candles we consider to know if the price rises or fall
+
 in_ = int(
     input(
         "Enter the number of candels (Y) to consider in the model for the prediction: \n"
     )
 )
+
 # Number of candels to consider for the prediction
+
 rows = int(
     input(
         "Enter the number of candels (X) consider in the model for the prediction: \n"
     )
 )
-#The relative strength index (RSI) is a momentum indicator
+
+# The relative strength index (RSI) is a momentum indicator
 #  used in technical analysis that measures the magnitude 
 # of recent price changes to evaluate overbought or oversold 
 # conditions in the price of a stock or other asset 
 
-rsi_ = int(input("Enter the rsi value to consider (30 recomended): \n"))
+rsi_compare = int(input("Enter the rsi value to consider (30 recomended): \n"))
 
 # The number of the previous candlesticks (periods) is the main setting 
 # of the indicator called a period. By default Period = 14; this is the value the author used.
@@ -45,19 +51,34 @@ rsi_ = int(input("Enter the rsi value to consider (30 recomended): \n"))
 periods = int(
     input("Enter the amount of periods for rsi calculation (14 recomended): \n")
 )
+
 p = rows + in_
 
-a = int(input("Enter how much to increase the mean volume value: \n"))
+# slope_ will be used to compare the candels slope (if it is negative is falling
+#  and if it is positive is rising) 
 
 slope_ = float(input("Enter the slope to take in reference, (0 recomended):\n"))
 
+# temp is the interval to consider, ex: 1d or 1h or 30m or 15m or 5m for each candlestick
+
 temp = input("Enter the interval to consider, ex: 1d or 1h or 30m or 15m or 5m \n")
 
+# vol_p is the number that will be used to calculate the volume mean
+
 vol_p = int(input("Enter how many candels consider to calculate the volume mean: \n"))
+
+# a will allow us know if the volume of the candels is "a" times bigger than the mean volume
+# of the previus candels
+
+a = int(input("Enter how much to increase the mean volume value: \n"))
+
 # List that'll be filled with the different Crypto-currency used
+
 names = ["LISTA:"]
 
-# counter uesd in the main program, 
+# counter is used in the main program to count the number of historical Crypto-currency data
+# added as argument
+
 count = 0
 
 ######################################################################################################
@@ -66,26 +87,70 @@ count = 0
 ######################################################################################################
 # MAIN PROGRAM 
 
+# This for will read each historical Crypto-currency data added as an argument
+
 for nam in range(1, len(sys.argv)):
+
+    # file is a DataFrame created since the csv file with the historical Crypto-currency data
+
     file = pd.read_csv(f"base/{str(sys.argv[nam])}_{temp}_base.csv")
 
+    # rsi is a list with each candel RSI value
+
     rsi = RSI(file["close"], periods)
+
+    # Here the RSI list is added to the dataframe file
+
     file["rsi"] = rsi
+
+    # The macd function receives a DataFrame and add to it the
+    # macd, macd_h and macd_s columns
+
     file = macd(file)
-    file.drop(index=file.index[:95], axis=0, inplace=True)
+
+    # Here we delete the first 95 columns because the firts RSI values are 
+    # erroneous
+
+    file.drop(index = file.index[:95], axis=0, inplace=True)
+
+    # After 
+
     file = file.reset_index()
 
+    # if it is the first historical Crypto-currency data, then:
+
     if count == 0:
-        v = verify(file, vol_p)
+
+        # The funtion that generates the rows with the sequence of candels
+        # named verify is called.
+        # verify requires the DataFrame with the historical Crypto-currency data
+        # and returns a DataFrame where each row is a sequence of candels determinated (rows), its
+        #  technical indicators (rsi,macd, etc), other parameters like slope and finaly if the consecutives candels (in_)
+        # have shown an increase or not (buy_decide) of its value in a the determinated percent (B) 
+
+        v = verify(file)
+
         count += 1
+
+    # if it's not the first historical Crypto-currency data, then:
+
     else:
-        k = verify(file, vol_p)
+
+        k = verify(file)
+
+        # After we call verify, we append the new DataFrame of the new historical Crypto-currency data
+        # to the previusly generated.
 
         v = pd.concat([v, k], ignore_index=True)
 
+    # add the name of the historical Crypto-currency in the names list
+
     names.append(str(sys.argv[nam]))
+
 y = v.buy
+
 # columns to use to run the rfc
+
 features = name_col(rows)
 X = v[features]
 X_train, X_test, y_train, y_test = train_test_split(
@@ -101,45 +166,97 @@ filename = f"model_p_{p}_perio_{periods}_in_{in_}_{B}_{st[0:16]}.sav"
 pickle.dump(rfc, open(filename, "wb"))
 
 ######################################################################################################
-######################################################################################################
+# RESULTS
+# name of the .sav file
 
-def verify(file, vol_p=0):
+print(f"model_p_{p}_perio_{periods}_in_{in_}_{B}_{st[0:16]}.sav")
+
+# name of the historical Crypto-currency data used
+
+print(names)
+
+# The classification report allows us to compare the precission, aquracy of different variables 
+# used like B, in_, a also the different historical Crypto-currency data.
+
+print(classification_report(y_test, predictions))
+
+# The next information is useful to know which variables were used to obtain 
+# what the classification report indicates
+
+print(f"periods rsi: {periods}")
+print(f"Y candels: {in_}")
+print(f"Amount that increased the volume: {a}")
+print(f"rows: {rows}")
+print(f"Candels used to calculate volume: {vol_p}")
+
+######################################################################################################
+# FUNCTIONS
+
+def verify(file):
     # create the index of the df
     index_ = name_col(rows)
     index_.append("date")
     index_.append("buy")
     k = pd.DataFrame(columns=index_)
-    # Generate a list to analize the slope with linear regression from cero to rows
+
+    # Generate the  x-axis list to analize the slope with linear regression from cero to rows
+
     X = [x for x in range(0, p - in_)]
+
     for i in range(p + vol_p, len(file.index)):
+
+        # Generate the y-axis list to analize the slope with linear regression from cero to rows candels close values
+
         Y = [file["close"][t] for t in range(i - p, i - in_)]
+
+        # Here creates a list with the RSI of the candels involved to find the minimun one 
+        # and compare with the stablished before
+
         rsi_list = [file["rsi"][t] for t in range(i - p, i - in_)]
+
+        # The slope of the candels is calculated
+
         slope, intercept, r_value, p_value_2, std_err = linregress(X, Y)
+
+        # Mean volume calculate
+
         vol = [file["volume"][i - x] for x in range(in_, p + 1 + vol_p)]
+
         vol_prom = np.mean(vol)
-        si_no = 1
-        # if the values in the row pass the filter, they are considered to make the predictor model
+
+        buy_decide = 1
+
+        # If the values in the row pass the filter, they are considered to make the predictor model
+        # The slope, the volume and the rsi are compared
+
         if (
             slope < slope_
             and (file["volume"][i - in_] or file["volume"][i - in_ - 1]) > vol_prom * a
-            and min(rsi_list) < rsi_
+            and min(rsi_list) < rsi_compare
         ):
             # hi is the list with variaton of the candles we consider to know if the price rises or fall
-            hi = [
-                (file["high"][i - t] - file["close"][i - in_]) / file["close"][i - in_]
-                for t in range(in_ + 1)
-            ]
-            if max(hi) > B:
-                si_no = f"{B}"
+
+            high_candel = [(file["high"][i - t] - file["close"][i - in_]) / file["close"][i - in_] for t in range(in_ + 1)]
+
+            # If the candles that come after to the first candles considered (rows) 
+            # are bigger than the B established value, buy decide will be B, otherwise it will be 0
+
+            if max(high_candel) > B:
+
+                buy_decide = f"{B}"
+
             else:
-                si_no = "0"
-        # either cero or B, if the row was considered, is will be added to the df to then execute the predictor
-        if si_no != 1:
+
+                buy_decide = "0"
+
+        # either cero or B, if the row was considered, is will be added to the DataFrame to then execute the predictor
+
+        if buy_decide != 1:
+
             row = list()
+
             for t in range(in_, p + 1):
                 row = row + [
-                    # file["open"][i-t]/file["low"][i-p],file["close"][i-t]/file["low"][i-p],
-                    # file["high"][i-t]/file["low"][i-p], file["low"][i-t]/file["low"][i-p],
                     file["volume"][i - t] / float(file["volume"][i - p]),
                     (file["open"][i - t] - file["close"][i - t]) / file["low"][i - t],
                     (file["close"][i - t] - file["open"][i - t]) / file["high"][i - t],
@@ -149,17 +266,9 @@ def verify(file, vol_p=0):
                     file["macd_h"][i - t],
                     file["macd_s"][i - t],
                 ]
-            row = row + [file["date"][i - in_], si_no]
+            row = row + [file["date"][i - in_], buy_decide]
             k.loc[len(k.index)] = row
         k = k.dropna()
     return k
 
-print(f"model_p_{p}_perio_{periods}_in_{in_}_{B}_{st[0:16]}.sav")
-print(names)
-print(classification_report(y_test, predictions))
-print(f"periods rsi: {periods}")
-print(f"Y candels: {in_}")
-print(f"p: {p}")
-print(f"a: {a}")
-print(f"rows: {rows}")
-print(f"volume: {vol_p}")
+
