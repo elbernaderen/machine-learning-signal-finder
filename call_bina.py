333421bfc91d139e4d5a6ba475282ebdb56dc2fa
elbernaderen <65098903+elbernaderen@users.noreply.github.com>
2022-07-05 08:19:06 -0300

    add modificactations

--- call_bina.py
+++ call_bina.py
@@ -4,12 +4,15 @@ import datetime
 import time
 
 if len(sys.argv) > 0:
-    nam = sys.argv[1]
-    name = sys.argv[2]
-    año = int(sys.argv[3])
-    mes = int(sys.argv[4])
-    dia = int(sys.argv[5])
+    name = sys.argv[1]
+    file_name = sys.argv[2]
+    year = int(sys.argv[3])
+    month = int(sys.argv[4])
+    day = int(sys.argv[5])
 print(dia)
-start_date = datetime.datetime(año, mes, dia)
-kk = store_ohlcv(symbol=nam, interval="30m", start_date=start_date, name=name)
-print(nam)
+start_date = datetime.datetime(year, month, day)
+kk = store_ohlcv(symbol = name,
+                interval = "30m",
+                start_date = start_date,
+                 name = file_name)
+print(name)
